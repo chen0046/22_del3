3f75cd447baf4a2af5f1f0ef90f5887cdcad330f
chen <s205420@student.dtu.dk>
2020-11-11 11:01:48 +0100

    test gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
 import gui_main.GUI;
 
 public class Main {
     public static void main(String[] args) {
         GUI gui = new GUI();
+
+        GUI_Player player = new GUI_Player("Stephen", 2000);
+        gui.addPlayer(player);
+        GUI_Field field = gui.getFields()[4];
+        field.setCar(player, true);
+
+
     }
 }
