c69b89ac2ad8fbd1623c9717bce9e639031accf7
Oliver <o.lyngholm@gmail.com>
2020-11-12 22:41:34 +0100

    Næsten færdigt spillebræt plus makeFields metode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,8 @@
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Player;
-import gui_main.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
-
-        GUI_Player player = new GUI_Player("Stephen", 2000);
-        gui.addPlayer(player);
-        GUI_Field field = gui.getFields()[4];
-        field.setCar(player, true);
-
+        Fields game = new Fields();
+        game.makeFields();
 
     }
 }
