08c973ba53276420405fe0f2fbfa5f4c44288952
Oliver <o.lyngholm@gmail.com>
2020-11-13 12:15:08 +0100

    SpillerController

--- src/main/java/Spiller.java
+++ src/main/java/Spiller.java
@@ -1,14 +1,24 @@
+import gui_fields.GUI_Player;
+
 import java.util.Scanner;
 
 public class Spiller {
+
     String navn; //Navn skal defineres som String da det involverer tekst
     Scanner input = new Scanner(System.in); //Vi tilføjer Scanneren så vi kan modtage inputs fra spilleren
-    Konto spillerKonto = new Konto(); //Her sørger vi for at hver spiller har sin egen konto
+    Konto spillerKonto;
+    int pos;
+    int Birthday;
 
-    /* Her sørger vi for at når "askname" bliver kaldt på i andre klasser, så bliver det næste input spilleren laver
+
+
+
+    /*
+    Her sørger vi for at når "askname" bliver kaldt på i andre klasser, så bliver det næste input spilleren laver
     det navnet bliver sat til */
+
     public void askName(Spiller currentPlayer) {
-        currentPlayer.setNavn(input.nextLine());
+
     }
 
     //Her gør vi så når man skriver getNavn så får man det definerede Navn
