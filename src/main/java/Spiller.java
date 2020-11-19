5bddf46c286480292aab5c1c1def178497d2748f
Oliver <o.lyngholm@gmail.com>
2020-11-19 14:06:36 +0100

    Start, long way to go

--- src/main/java/Spiller.java
+++ src/main/java/Spiller.java
@@ -1,21 +1,27 @@
-import gui_fields.GUI_Player;
 
-import java.util.Scanner;
 
 public class Spiller {
 
     String navn; //Navn skal defineres som String da det involverer tekst
-    Scanner input = new Scanner(System.in); //Vi tilføjer Scanneren så vi kan modtage inputs fra spilleren
     Konto spillerKonto;
     int pos;
     int Birthday;
 
+    public int getPos() {
+        return pos;
+    }
 
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
 
+    public int getBirthday() {
+        return Birthday;
+    }
 
-    /*
-    Her sørger vi for at når "askname" bliver kaldt på i andre klasser, så bliver det næste input spilleren laver
-    det navnet bliver sat til */
+    public void setBirthday(int birthday) {
+        Birthday = birthday;
+    }
 
     //Her gør vi så når man skriver getNavn så får man det definerede Navn
     public String getNavn() {
--- src/main/java/SpillerController.java
+++ src/main/java/SpillerController.java
@@ -1,6 +1,28 @@
+import gui_fields.GUI_Player;
+
 public class SpillerController {
 
+    Spiller[] spillere;
+    int spillerAntal;
+    GUI_Player[] gui_players;
+
+
+    public SpillerController() {
+        spillere = new Spiller[spillerAntal];
 
     }
+
+
+    public Spiller[] getSpillere() {
+        return spillere;
+    }
+
+    public void setSpillere(Spiller[] spillere) {
+
+    }
+
+
+
+}
 
 
