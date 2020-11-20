bad90af02038425e6ccf5561e222d4441308bf51
chen <s205420@student.dtu.dk>
2020-11-20 13:14:13 +0100

    start på Consol

--- src/main/java/Consol.java
+++ src/main/java/Consol.java
@@ -1,4 +1,17 @@
+import gui_main.GUI;
+
 public class Consol {
+    BoardController boardController = new BoardController();
+    GUI gui = new GUI(boardController.getGui_fields());
+    Terning terning = new Terning();
+
+    public void Startgame(){
+        while(true) {
+            gui.getUserButtonPressed("hej", "slå terningen");
+            gui.setDie(terning.henttotal());
+        }
+    }
+
 
 
 }
--- src/main/java/Klasser.java
+++ src/main/java/Klasser.java
@@ -2,9 +2,9 @@ import gui_main.GUI;
 
 public class Klasser {
     public static void main(String[] args) {
-       // Fields game = new Fields();
-       // game.makeFields();
-        BoardController boardController = new BoardController();
-        GUI gui = new GUI(boardController.getGui_fields());
+       Consol consol = new Consol();
+       consol.Startgame();
+
     }
+
 }
